2368e9a14166bfc9138d84afe2578f9991047297
Maisha589 <maisharahman589@gmail.com>
2022-05-02 05:39:27 +0600

    added steps

---
+++ _steps.js
@@ -0,0 +1,24 @@
+/**
+ * Basic Server Setup
+ * 1. Create a folder (manually or mkdir)
+ * 2. Open command line tot that folder
+ * 3. yarn init
+ * 4. yarn add express 
+ * 5. create index.js file
+ * -------------------------------------
+ * For express/node server
+ * ------------------------------------
+ * 1. require express
+ * 2. create app variable
+ * 3.declare port
+ * 4.set app.get('/')
+ * 5. listen to port
+ * 6.node index.js
+ * 7.Check your browser for that port
+ * --------------
+ * get
+ * -------------------
+ * 1.cors middleware
+ * Post
+ * ----------------------
+ * */
